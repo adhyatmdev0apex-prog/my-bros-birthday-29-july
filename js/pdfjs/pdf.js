8d1d003a5f2b2f71103c831e32c0cf88044396a9
adhyatmdev0apex-prog <adhyatm.dev0apex@gmail.com>
2026-07-27 13:29:37 +0000

    Move local PDF.js into project folders

--- js/pdfjs/pdf.js
+++
@@ -1,5 +0,0 @@
-/*
-  This file should contain the full UMD build of PDF.js.
-  It will define `window.pdfjsLib` for other scripts to use.
-*/
-console.log("Local PDF.js library loaded.");
--- js/pdfjs/pdf.worker.js
+++
@@ -1,5 +0,0 @@
-/*
-  This file should contain the full UMD worker build of PDF.js.
-  It is loaded by the main PDF.js library.
-*/
-console.log("Local PDF.js worker loaded.");
